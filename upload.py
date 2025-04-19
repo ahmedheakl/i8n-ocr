d161db2cef0276acf2f408c2824e7771ca2e07bd
Ahmed Heakl <52796111+ahmedheakl@users.noreply.github.com>
2025-04-19 10:53:50 +0400

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,6 +4,7 @@ from PIL import Image
 from tqdm import tqdm
 import os
 import dotenv
+import json
 
 dotenv.load_dotenv()   
 
@@ -12,33 +13,55 @@ images_dir = "layouts"
 doctag_html_dir = "doctag_html_outputs"
 doctag_otsl_dir = "doctag_otsl_outputs"
 markdown_dir = "markdown_outputs"
+languages_dir = "languages"
 images = glob(f"{data_dir}/{images_dir}/*")
 
 
+def read_json(path):
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
 def read(path):
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
-    except Exception as e:
-        print(f"Error reading {path}: {e}")
-        return ""
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
 data = {
     "id": [],
     "image": [],
     "doctag_html": [],
     "doctag_otsl": [],
     "markdown": [],
+    "language": [],
+    "language_confidence": [],
 }
+
+collected_langs = {}
 for img in tqdm(images):
     img_name = os.path.basename(img).replace(".jpg", "")
+    image = Image.open(img)
+    try:
+        doctag_html = read(os.path.join(data_dir, doctag_html_dir, img_name + ".dt.xml"))
+        doctag_otsl = read(os.path.join(data_dir, doctag_otsl_dir, img_name + ".dt.xml"))
+        markdown = read(os.path.join(data_dir, markdown_dir, img_name + ".md"))
+        language_json = read_json(os.path.join(data_dir, languages_dir, img_name + ".json"))
+    except Exception as e:
+        print(f"Error reading files for {img_name}: {e}")
+        continue
+    language = language_json.get("language", "unknown")
+    if language not in collected_langs:
+        collected_langs[language] = 0
+    if collected_langs[language] > 10:
+        continue
+    language_conf = language_json.get("confidence", 1.0)
     data["id"].append(img_name)
-    data["image"].append(Image.open(img))
-    doctag_html = os.path.join(data_dir, doctag_html_dir, img_name + ".dt.xml")
-    doctag_otsl = os.path.join(data_dir, doctag_otsl_dir, img_name + ".dt.xml")
-    markdown = os.path.join(data_dir, markdown_dir, img_name + ".md")
-    data["doctag_html"].append(read(doctag_html))
-    data["doctag_otsl"].append(read(doctag_otsl))
-    data["markdown"].append(read(markdown))
+    data["image"].append(image)
+    data["doctag_html"].append(doctag_html)
+    data["doctag_otsl"].append(doctag_otsl)
+    data["markdown"].append(markdown)
+    data['language'].append(language)
+    data['language_confidence'].append(language_conf)
+    collected_langs[language] = collected_langs.get(language, 0) + 1
 dataset = datasets.Dataset.from_dict(data)
 dataset = dataset.cast_column("image", datasets.Image())
 dataset = dataset.cast_column("doctag_html", datasets.Value("string"))
@@ -46,4 +69,4 @@ dataset = dataset.cast_column("doctag_otsl", datasets.Value("string"))
 dataset = dataset.cast_column("markdown", datasets.Value("string"))
 dataset = dataset.cast_column("id", datasets.Value("string"))
 
-dataset.push_to_hub("ahmedheakl/i8n-ocr")
+dataset.push_to_hub("ahmedheakl/i8n-ocr-bench")
